2a59f0698bfe08d9f2c6617b21aee964e07ddfd0
Steven <38477361+Steven-matos@users.noreply.github.com>
2020-07-08 19:14:27 -0400

    finished histo

--- applications/histo/histo.py
+++ applications/histo/histo.py
@@ -1,2 +1,20 @@
 # Your code here
 
+with open("robin.txt") as f:
+    words1 = f.read()
+    words = words1.split()
+
+cache = {}
+
+for word in words:
+    if word not in cache:
+        cache[word] = '#'
+    else:
+        cache[word] += '#'
+
+items = list(cache.items())
+
+items.sort(key=lambda x: x[1], reverse=True)
+
+for key, value in items:
+    print(f'{key:<18} {value}')
