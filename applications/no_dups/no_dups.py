fccec24d0eaff279d58e4e4b3644b772f79132ee
Steven <38477361+Steven-matos@users.noreply.github.com>
2020-07-09 14:16:13 -0400

    finished no dup

--- applications/no_dups/no_dups.py
+++ applications/no_dups/no_dups.py
@@ -1,6 +1,20 @@
 def no_dups(s):
     # Your code here
+    cache = {}
+    strs = s.split()
 
+    for c in strs:
+        if c not in cache:
+            cache[c] = 1
+        else:
+            cache[c] += 1
+    cache.keys()
+
+    str1 = " "
+    list_a = list(cache.keys())
+    print(list_a)
+
+    return str1.join(list_a)
 
 
 if __name__ == "__main__":
